21da7d9a5238b3ae36cde0a82a6d2eaa7adc1cc4
ramtinkh <ramtinkhoshnevis1@gmail.com>
2024-02-27 11:45:26 +0330

    fix: fix App data

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Salam Mamad!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://taupe-stroopwafel-e719ad.netlify.app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bezan Bargardi haminja
         </a>
       </header>
     </div>
